3a82dbcccf863460a414d7f9bc592b41b31900ca
Alyona <tyoaa51@gmail.com>
2024-01-13 21:01:01 +0600

    Create a project and a blogapp

---
+++ dproject/blogapp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Post(models.Model):
+    """Post data"""
+    title = models.CharField('Post Title', max_length=100)
+    description = models.TextField('Post text')
+    author = models.CharField('Author name', max_length=100)
+    date = models.DateTimeField('Publication date')
+    img = models.ImageField('Image', upload_to='image/%Y')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    def get_summary(self):
+        words = self.description.split()
+        return f'{" ".join(words[:100])}...'
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+class Comments(models.Model):
+    email = models.EmailField()
+    name = models.CharField('Name', max_length=50)
+    comment_text = models.TextField('Comment', max_length=2000)
+    post = models.ForeignKey(Post, verbose_name='Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+
+class Likes(models.Model):
+    """Likes"""
+    ip = models.CharField('IP-address', max_length=100)
+    post = models.ForeignKey(Post, verbose_name='Post', on_delete=models.CASCADE)
